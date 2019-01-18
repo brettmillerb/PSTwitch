865514b6df5a1166b25c6b42a8789f4a507e8021
Brett Miller <brett@millerb.co.uk>
2019-01-18 21:44:53 +0000

    Tried to get pagination working
    Failed miserably for about 45 minutes. Revist

--- Get-TwitchFollowers.ps1
+++ Get-TwitchFollowers.ps1
@@ -1,7 +1,14 @@
 function Get-TwitchFollowers {
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory,
+            ParameterSetName = 'Standard',
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
         $UserName,
+
+        [switch]
+        $All,
 
         $Token = $Script:Token
     )
@@ -9,17 +16,26 @@ function Get-TwitchFollowers {
     $userId = Get-TwitchUser -UserName $UserName | Select-Object -ExpandProperty UserId
     $followersUri = "{0}/users/follows?to_id={1}" -f $Script:Uri, $userId
 
-    $followersUri | Write-Verbose
+    $invokeRestMethodSplat = @{
+        Method  = 'Get'
+        Headers = $Script:Headers
+        Uri     = $followersUri
+    }
 
-    $followersResult = Invoke-RestMethod -Uri $followersUri -Headers $Script:Headers -Method Get
+    $followersResult = Invoke-RestMethod @invokeRestMethodSplat
 
-    if ($followersResult.data) {
-        $followersResult.data | ForEach-Object {
-            [PSCustomObject]@{
-                UserName   = $_.from_name
-                UserId     = $_.from_id
-                FollowedAt = $_.followed_at
-            }
+    if ($All) {
+        while ($followersResult.data) {
+            $followLinkUri = "{0}&after={1}" -f $Script:Uri, $followersResult.pagination.cursor
+            $followLinkResult = Invoke-RestMethod -Uri $followLinkUri -Headers $Script:Headers
+            $null = $followersResult.Add($followLinkResult)
+        }
+    }
+    $followersResult.data | ForEach-Object {
+        [PSCustomObject]@{
+            UserName   = $_.from_name
+            UserId     = $_.from_id
+            FollowedAt = $_.followed_at
         }
     }
 }
