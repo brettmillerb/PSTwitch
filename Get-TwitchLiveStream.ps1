b17af882e276782c9270bb6735e244cd68b47828
Brett Miller <brett@millerb.co.uk>
2019-01-18 20:39:32 +0000

    Added Pipeline support to Get-TwitchLiveStream

--- Get-TwitchLiveStream.ps1
+++ Get-TwitchLiveStream.ps1
@@ -1,23 +1,37 @@
 function Get-TwitchLiveStream {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Standard')]
     param (
+        [Parameter(Mandatory,
+            ParameterSetName = 'Standard')]
         $UserName,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'Pipeline',
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [psobject]
+        $InputObject,
+
         $Token = $Script:Token
     )
 
-    $streamsUri = "{0}/streams?user_login={1}" -f $Script:Uri, $UserName
-
-    $streamsResults = Invoke-RestMethod -Uri $streamsUri -Headers $script:Headers
-
-    if ($streamsResults.data) {
-        [PSCustomObject]@{
-            UserName    = $streamsResults.data.user_name
-            StreamTitle = $streamsResults.data.title
-            ViewerCount = $streamsResults.data.viewer_count
-            StartedAt   = $streamsResults.data.started_at
+    process {
+        foreach ($user in $InputObject) {
+            $streamsUri = "{0}/streams?user_login={1}" -f $Script:Uri, $user.UserName
+        
+            $streamsResults = Invoke-RestMethod -Uri $streamsUri -Headers $script:Headers
+        
+            if ($streamsResults.data) {
+                [PSCustomObject]@{
+                    UserName    = $streamsResults.data.user_name
+                    StreamTitle = $streamsResults.data.title
+                    ViewerCount = $streamsResults.data.viewer_count
+                    StartedAt   = $streamsResults.data.started_at
+                }
+            }
+            else {
+                "No Live Stream Available"
+            }
         }
-    }
-    else {
-        "No Live Stream Available"
     }
 }
