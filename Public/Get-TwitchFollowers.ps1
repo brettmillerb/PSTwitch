459d12641d6fda12ccc71bc4202632adfe745766
Brett Miller <brett@millerb.co.uk>
2019-02-09 23:15:17 +0000

    Added pipeline support for Get-TwitchFollowers

--- Public/Get-TwitchFollowers.ps1
+++ Public/Get-TwitchFollowers.ps1
@@ -1,11 +1,16 @@
 function Get-TwitchFollowers {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Standard')]
     param (
+        [parameter(Mandatory,
+            ParameterSetName = 'Standard')]
+        [string]
+        $UserName,
+
         [Parameter(Mandatory,
-            ParameterSetName = 'Standard',
+            ParameterSetName = 'Pipeline',
             ValueFromPipeline,
             ValueFromPipelineByPropertyName)]
-        $UserName,
+        $InputObject,
 
         [switch]
         $All,
@@ -13,38 +18,41 @@ function Get-TwitchFollowers {
         $Token = $Script:Token
     )
 
-    $userId = Get-TwitchUser -UserName $UserName | Select-Object -ExpandProperty UserId
-    $followersUri = "{0}/users/follows?to_id={1}" -f $Script:Uri, $userId
-
-    $invokeRestMethodSplat = @{
-        Method  = 'Get'
-        Headers = $Script:Headers
-        Uri     = $followersUri
-    }
-
-    $followersResult = [System.Collections.Generic.List[psobject]]::new()
-    $results = Invoke-RestMethod -Uri $followersUri -Headers $Script:Headers
-    $cursor = $results.pagination.cursor
-    $followersResult.Add($results.data)
-    if ($all) {
-        do {
-            $cursorURI = "{0}&after={1}" -f $followersURI, $cursor
-            $results = Invoke-RestMethod -Uri $cursorURI -Headers $script:Headers
-            $followersResult.Add($results.data)
-            $cursor = $results.pagination.cursor
-            if (-not $cursor) {
-                break
-            }
+    process {
+        if ($PSCmdlet.ParameterSetName -eq 'Pipeline') {
+            $userId = Get-TwitchUser -UserName $_.UserName
+            $followersUri = "{0}/users/follows?to_id={1}" -f $Script:Uri, $_.UserId
         }
-        until ($followersResult.Count -eq $Results.total)
-    }
+        else {
+            $userId = Get-TwitchUser -UserName $UserName | Select-Object -ExpandProperty UserId
+            $followersUri = "{0}/users/follows?to_id={1}" -f $Script:Uri, $userId
+        }
 
-    $followersResult | ForEach-Object {
-        $_ | Select-Object -property @(
-            @{name='FollowerUserId';expression={$_.from_id}},
-            @{name='FollowerUserName';expression={$_.from_name}},
-            @{name='UserName';expression={$_.to_name}},
-            @{name='FollowedOn';expression={$_.followed_at}}
-        )
+        $followersResult = [System.Collections.Generic.List[psobject]]::new()
+        $results = Invoke-RestMethod -Uri $followersUri -Headers $Script:Headers
+        $cursor = $results.pagination.cursor
+        $followersResult.Add($results.data)
+        if ($all) {
+            do {
+                $cursorURI = "{0}&after={1}" -f $followersURI, $cursor
+                $results = Invoke-RestMethod -Uri $cursorURI -Headers $script:Headers
+                $followersResult.Add($results.data)
+                $cursor = $results.pagination.cursor
+                if (-not $cursor) {
+                    break
+                }
+            }
+            until ($followersResult.Count -eq $Results.total)
+        }
+    }
+    end {
+        $followersResult | ForEach-Object {
+            $_ | Select-Object -property @(
+                @{name='FollowerUserId';expression={$_.from_id}},
+                @{name='FollowerUserName';expression={$_.from_name}},
+                @{name='UserName';expression={$_.to_name}},
+                @{name='FollowedOn';expression={$_.followed_at}}
+            )
+        }
     }
 }
