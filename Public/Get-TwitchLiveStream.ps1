5e7f5b1da100ce45048e5683dfbf1a7c69c49663
Brett Miller <brett@millerb.co.uk>
2019-02-09 00:51:57 +0000

    Merge Dev (#2)
    * Added Pipeline support to Get-TwitchLiveStream
    
    * Tried to get pagination working
    
    Failed miserably for about 45 minutes. Revist
    
    * Updated to get all followers per user
    
    * Added manifest, readme, build, public folder
    
    * Added Support for both parameter sets

--- Public/Get-TwitchLiveStream.ps1
+++ Public/Get-TwitchLiveStream.ps1
@@ -16,11 +16,30 @@ function Get-TwitchLiveStream {
     )
 
     process {
-        foreach ($user in $InputObject) {
-            $streamsUri = "{0}/streams?user_login={1}" -f $Script:Uri, $user.UserName
-        
+
+        if ($PSCmdlet.ParameterSetName -eq 'Pipeline') {
+            foreach ($user in $InputObject) {
+                $streamsUri = "{0}/streams?user_login={1}" -f $Script:Uri, $user.UserName
+
+                $streamsResults = Invoke-RestMethod -Uri $streamsUri -Headers $script:Headers
+
+                if ($streamsResults.data) {
+                    [PSCustomObject]@{
+                        UserName    = $streamsResults.data.user_name
+                        StreamTitle = $streamsResults.data.title
+                        ViewerCount = $streamsResults.data.viewer_count
+                        StartedAt   = $streamsResults.data.started_at
+                    }
+                }
+                else {
+                    "No Live Stream Available"
+                }
+            }
+        }
+        else {
+            $streamsUri = "{0}/streams?user_login={1}" -f $Script:Uri, $UserName
             $streamsResults = Invoke-RestMethod -Uri $streamsUri -Headers $script:Headers
-        
+
             if ($streamsResults.data) {
                 [PSCustomObject]@{
                     UserName    = $streamsResults.data.user_name
